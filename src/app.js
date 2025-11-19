a8147cfe435ff7b6f9e4b53188aeee4651cf509b
Benji <bendvg@gmail.com>
2025-11-19 21:01:47 +0000

    DONE!

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let determiner = ['a', 'my', 'your', 'the', 'our'];
+let descriptiveAdj = ['fast', 'super', 'best', 'creative','great', 'big'];
+let objectNoun = ['site', 'app', 'solution', 'idea'];
+let dominioGenerico = [".com", ".org", ".net", ".gov"];
+
+for (let i = 0 ; i < determiner.length; i++) {
+  for (let j = 0 ; j < descriptiveAdj.length; j++) {
+    for (let k = 0 ; k < objectNoun.length; k++) {
+      for (let l = 0 ; l < dominioGenerico.length; l++) {
+        console.log(determiner[i] + descriptiveAdj[j] + objectNoun[k] + dominioGenerico[l]);
+      }
+    }
+  }
+}
 };
